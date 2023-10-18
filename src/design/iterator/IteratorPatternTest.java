41954263ed719710e79784ab6232e85cb2037ef2
luiscspalmeira <lcspalmeira@yahoo.com.br>
2023-10-18 18:24:34 -0300

    finalizacao

--- src/design/iterator/IteratorPatternTest.java
+++ src/design/iterator/IteratorPatternTest.java
@@ -1,5 +1,14 @@
 package design.iterator;
 
+/*
+ * 
+ * Exemplo de design pattern de Iterator
+ * 
+ * exemplo usado do site
+ * https://www.digitalocean.com/community/tutorials/iterator-design-pattern-java
+ */
+
+
 public class IteratorPatternTest {
 
 	public static void main(String[] args) {
